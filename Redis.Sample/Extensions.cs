7d5f8a40d2203c7cf634b9725f7c57cdbd728356
SergerGood <serzher@inbox.ru>
2020-10-23 09:33:51 +0300

    samples

---
+++ Redis.Sample/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace Redis.Sample
+{
+    public static class Extensions
+    {
+        public static string GetString(this byte[] value)
+        {
+            return Encoding.UTF8.GetString(value);
+        }
+
+        public static byte[] GetBytes(this string value)
+        {
+            return Encoding.UTF8.GetBytes(value);
+        }
+    }
+}
